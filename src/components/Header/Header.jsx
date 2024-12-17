55762adb0b3120b28d19ae404b42657a475d5d06
otabek-art <pocani497@gmail.com>
2024-12-17 14:58:22 +0300

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,78 +1,82 @@
-import React, {useState} from 'react'
-import './Header.scss'
-import { Link } from 'react-router-dom'
-import { useAppContext } from '../../context'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useAppContext } from '../../context';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './Header.scss';
 
 const Header = () => {
+  const { isActive, toggleBurger, user, isAuth, logout } = useAppContext();
 
-  const{ isActive , toggleBurger, user,isAuth , logout } = useAppContext()
+  // Инициализация AOS
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Длительность анимации
+      once: true,     // Запуск анимации только один раз
+    });
+  }, []);
 
-
-
-
-  
   return (
-    <>
-    <header className="header">
-        <div className="container">
-            <div className='header__wrapper'>
-              <div className="header__wrap">
-              <div className="header__header">
-              <Link to={'/'}><img className='logo__img' src="/SHOP.CO.svg" alt="" /></Link>
-                <div className={`header__menu ${isActive ? 'active' : ''}`}>
-                    <Link to={'/sale'} className='dropdown' >
-                    Shop
-                    <img  src="https://www.iconpacks.net/icons/2/free-arrow-down-icon-3101-thumb.png" alt="" />
-
-                    <div>
-                      <Link to={'/'}>Men</Link>
-                      <Link to={'/'}>Women</Link>
-                      <Link to={'/'}>Kids</Link>
-
-                    </div>
-                    </Link>
-
-                    <a href="/#selling">On Sale</a>
-                    <a href="/#arrivals">New Arrivals</a>
-                    <Link to={'/brands'}>Brands</Link>
-            </div>                
-              </div>
-
-            <div onClick={toggleBurger} className={`burger ${isActive ? 'active' : ''}`}>
-                <div className='div'></div>
-                <div className='div'></div>
-                <div className='div'></div>
-            </div>                
-              </div>
-
-                <div className="header__search">
-                  <div className='header__input'>
-                    <img src="/search-icon.svg" alt="" />
-                   <p>Search for products...</p>
-                   </div>
-                  <img src="/icon-1.svg" alt="" />
-             
-             {isAuth ? (
-              <div>
-        <b>{user.username}</b>  <br />
-        <button onClick={logaut} >logaut</button>
-              </div>
-      
-              
-             ):(
-              <Link to={"/login"}>
-              <img src="/icon-2.svg" alt="" />
+    <header className="header" data-aos="fade-down">
+      <div className="container">
+        <div className="header__wrapper" data-aos="fade-up">
+          <div className="header__wrap">
+            <div className="header__header" data-aos="zoom-in">
+              <Link to={'/'}>
+                <img className="logo__img" src="/SHOP.CO.svg" alt="Logo" />
               </Link>
-             )}
-             
-
-             
-                </div>
+              <div className={`header__menu ${isActive ? 'active' : ''}`} data-aos="fade-left">
+                <Link to={'/sale'} className="dropdown">
+                  Shop
+                  <img
+                    src="https://www.iconpacks.net/icons/2/free-arrow-down-icon-3101-thumb.png"
+                    alt="Dropdown Icon"
+                  />
+                  <div>
+                    <Link to={'/'}>Men</Link>
+                    <Link to={'/'}>Women</Link>
+                    <Link to={'/'}>Kids</Link>
+                  </div>
+                </Link>
+                <a href="/#selling">On Sale</a>
+                <a href="/#arrivals">New Arrivals</a>
+                <Link to={'/brands'}>Brands</Link>
+              </div>
             </div>
-        </div>
-    </header>
-    </>
-  )
-}
 
-export default Header
+            <div
+              onClick={toggleBurger}
+              className={`burger ${isActive ? 'active' : ''}`}
+              data-aos="flip-left"
+            >
+              <div className="div"></div>
+              <div className="div"></div>
+              <div className="div"></div>
+            </div>
+          </div>
+
+          <div className="header__search" data-aos="fade-in">
+            <div className="header__input">
+              <img src="/search-icon.svg" alt="Search Icon" />
+              <p>Search for products...</p>
+            </div>
+            <img src="/icon-1.svg" alt="Icon" />
+
+            {isAuth ? (
+              <div>
+                <b>{user.username}</b> <br />
+                <button onClick={logout}>Logout</button>
+              </div>
+            ) : (
+              <Link to={'/login'}>
+                <img src="/icon-2.svg" alt="Login Icon" />
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,37 +1,59 @@
-import React from 'react'
-import './Hero.scss'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './Hero.scss';
 
 const Hero = () => {
+  // Инициализация AOS
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Длительность анимации
+      once: true,     // Анимация запускается только один раз
+    });
+  }, []);
+
   return (
-  <>
-  <section className="hero">
-  <div className="container">
-    <div className="hero__wrapper">
-      <div className="hero__box">
-        <h1>FIND CLOTHES <br /> THAT MATCHES <br /> YOUR STYLE</h1>
-        <p className='hero__text'>Browse through our diverse range of meticulously crafted garments, designed <br /> to bring out your individuality and cater to your sense of style.</p>
-        <button>Shop Now</button>
-        <div className="hero__inner">
-          <div  >
-            <h3>200+</h3>
-            <p>International Brands</p>
-          </div>
-          <div>
-            <h3>2,000+</h3>
-            <p>High-Quality Products</p>
-          </div>
-          <div>
-            <h3>30,000+</h3>
-            <p>Happy Customers</p>
+    <>
+      <section className="hero">
+        <div className="container">
+          <div className="hero__wrapper">
+            {/* Анимация заголовка */}
+            <div className="hero__box" data-aos="fade-right">
+              <h1>
+                FIND CLOTHES <br /> THAT MATCHES <br /> YOUR STYLE
+              </h1>
+              <p className="hero__text">
+                Browse through our diverse range of meticulously crafted garments, designed <br />
+                to bring out your individuality and cater to your sense of style.
+              </p>
+              <button data-aos="zoom-in">Shop Now</button>
+              <div className="hero__inner" data-aos="fade-up">
+                <div>
+                  <h3>200+</h3>
+                  <p>International Brands</p>
+                </div>
+                <div>
+                  <h3>2,000+</h3>
+                  <p>High-Quality Products</p>
+                </div>
+                <div>
+                  <h3>30,000+</h3>
+                  <p>Happy Customers</p>
+                </div>
+              </div>
+            </div>
+            {/* Анимация изображения */}
+            <img
+              className="hero__img"
+              src="./hero-img.png"
+              alt="Hero"
+              data-aos="fade-left"
+            />
           </div>
         </div>
-      </div>
-      <img className='hero__img' src="./hero-img.png" alt="" />
-    </div>
-  </div>
-</section>
-  </>
-  )
-}
+      </section>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
